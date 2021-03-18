1e75ad1a87161f23f1057f8aa427a51557bb685f
charconst <44410850+charconst@users.noreply.github.com>
2021-03-17 21:04:59 -0700

    Added Tailwind CSS framework, setup to work with React

--- homebank_app/src/App.tsx
+++ homebank_app/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'tailwindcss/dist/tailwind.css'
 
 function App() {
   return (
     <div className="App">
+      <div className="p-6 max-w-sm mx-auto bg-yellow-400 rounded-xl shadow-md flex items-center space-x-4"></div>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
