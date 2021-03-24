c63069c02ba8a5a56e98dc8c1665070e23790407
charconst <44410850+charconst@users.noreply.github.com>
2021-03-24 14:39:57 -0700

    Added app header

--- homebank_app/src/App.tsx
+++ homebank_app/src/App.tsx
@@ -2,26 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'tailwindcss/dist/tailwind.css'
+import AppHeader from './AppHeader'
 
 function App() {
   return (
     <div className="App">
-      <div className="p-6 max-w-sm mx-auto bg-yellow-400 rounded-xl shadow-md flex items-center space-x-4"></div>
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {AppHeader()}
     </div>
   );
 }
