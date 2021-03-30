12de8cd6a5e561a5e99615cd14205e24e1d66f69
charconst <44410850+charconst@users.noreply.github.com>
2021-03-30 15:34:45 -0700

    Added basic Google OAuth Login on client

--- homebank_app/src/ui/pages/SignInPage.tsx
+++ homebank_app/src/ui/pages/SignInPage.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import GoogleLogin from 'react-google-login';
+
+const responseGoogle = (response:Object) => {
+    console.log(response);
+}
 
 function SignInPage() {
     return (
         <div>
             <h1>/signin</h1>
+            <GoogleLogin clientId="100265972375-l3fol743purv7qajo9en61in8815ukl2.apps.googleusercontent.com" 
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}
+            buttonText="Sign In with Google"/>
         </div>
     )
 }
