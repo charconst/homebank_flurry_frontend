b95e30336dece4f2a840fdc6adc6bd1775ca5a9f
charconst <44410850+charconst@users.noreply.github.com>
2021-03-31 20:06:44 -0700

    Added instructions text

--- homebank_app/src/ui/pages/Home.tsx
+++ homebank_app/src/ui/pages/Home.tsx
@@ -5,6 +5,24 @@ class Home extends React.Component {
         return (
             <div>
                 <h1>/ default route</h1>
+                <h2>Instructions</h2>
+                <p>
+                Listen carefully because you will only 
+              get one opportunity to listen.Be on the lookout for any 
+              clearly audible vocalization that is primarily communicative 
+              or playful, such as speech, babble, cooing, crying, sighing,
+              laughing, raspberries, clicks, etc. Please ignore vegetative 
+              sounds, such as heavy breathing, hiccups, sneezes, sniffles, 
+              and coughs, unless they appear to have been produced with a 
+              communicative or playful purpose. You will be asked to enter 
+              the codes (no spaces between) for the types of voices you 
+              heard in the segment that just played. Codes: t for child 
+              wearing the recorder, o for another child, a for adult. If 
+              there are multiple voices present, enter the multiple codes 
+              without spaces in between. Order of the codes does not matter.
+               Leave the space blank if you did not hear any communicative 
+              or playful vocalizations.
+                </p>
             </div>
         )
     }
