e4c1e929a4fa2992e3c54a05c7cac423b6a0750e
charconst <44410850+charconst@users.noreply.github.com>
2021-04-06 11:17:59 -0700

    Added audio clip container

---
+++ homebank_app/src/ui/components/AudioClipContainer.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class AudioClipContainer extends React.Component {
+    render() {
+        return (
+            <div className="rounded overflow-hidden shadow-lg text-center">
+                <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2">HomeBank Audio Clip</div>
+                <p className="text-gray-700 text-base">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                </p>
+            </div>
+            </div>
+        )
+    }
+}
+
+export default AudioClipContainer;
--- homebank_app/src/ui/pages/Home.tsx
+++ homebank_app/src/ui/pages/Home.tsx
@@ -1,25 +1,15 @@
 import React from 'react';
 import ReactAudioPlayer from 'react-audio-player';
+import AudioClipContainer from '../components/AudioClipContainer';
 
 class Home extends React.Component {
     render() {
         return (
-            <div>
+            <div className="container mx-auto">
                 <h1>/ default route</h1>
-                <ReactAudioPlayer controls src="https://storage.googleapis.com/homebank-public-audio/test.mp3"></ReactAudioPlayer>
-                <h2>Instructions</h2>
-                <p>
-                Listen carefully because you will only get one opportunity to listen.
-                You will be asked about the prominence within the clip of the voice 
-                of the child wearing the recorder (i.e. the target child) compared to 
-                all other sounds, such as other voices, background noise, rustling, etc.
-                A target child vocalization may include speech, singing, babble, crying,
-                trilling the lips, coughing, grunting, or any other sound produced
-                using the throat, lips, and/or tongue.
-                Note that the target child in this case is +ageYYMMDD[0:2]+ year(s), +ageYYMMDD[2:4]+ month(s), 
-                and +ageYYMMDD[4:6]+ day(s) old.
-                Press return to play the sound.
-                </p>
+                <AudioClipContainer></AudioClipContainer>
+                <AudioClipContainer></AudioClipContainer>
+                <AudioClipContainer></AudioClipContainer>
             </div>
         )
     }
