1f1c68e9e6e09d3805839450124ccfe780e72ac0
charconst <44410850+charconst@users.noreply.github.com>
2021-04-06 13:33:19 -0700

    Added progress bar to audio header

---
+++ homebank_app/src/ui/components/AudioClipProgressBar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class AudioClipProgressBar extends React.Component {
+    render() {
+        return (
+            <div className="flex flex-col">
+                <div className="relative top-0 h-4 rounded bg-indigo-800 w-1/2"></div>
+            </div>
+        );
+    }
+}
+
+export default AudioClipProgressBar;
--- homebank_app/src/ui/components/AudioClipProgressHeader.tsx
+++ homebank_app/src/ui/components/AudioClipProgressHeader.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import AudioClipProgressBar from '../components/AudioClipProgressBar';
 
 class AudioClipProgressHeader extends React.Component {
     render() {
         return (
             <div className="rounded overflow-hidden bg-indigo-500 text-center">
                 <div className="m-2"></div>
-                <div className="font-bold text-xl mb-2">1/1000 Clips Reviewed</div>
+                <div className="font-bold text-xl mb-2">500/1000 Clips Reviewed</div>
+                <AudioClipProgressBar></AudioClipProgressBar>
             </div>
         );
     }
