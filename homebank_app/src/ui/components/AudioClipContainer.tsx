3ec4b067299dd0b9a4b9d1d308e703302ee76659
charconst <44410850+charconst@users.noreply.github.com>
2021-04-06 12:07:51 -0700

    Renamed buttons to previous clip, next clip

--- homebank_app/src/ui/components/AudioClipContainer.tsx
+++ homebank_app/src/ui/components/AudioClipContainer.tsx
@@ -21,10 +21,10 @@ class AudioClipContainer extends React.Component {
                 <ReactAudioPlayer controls className="container mx-auto m-8"src="https://storage.googleapis.com/homebank-public-audio/test.mp3"></ReactAudioPlayer>
                 <div className="flex h-16 flex-wrap justify-center">
                     <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-8 border border-blue-500 hover:border-transparent rounded flex-initial m-2">
-                        Prev
+                        Previous Clip
                     </button>
                     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded flex-initial m-2">
-                        Next
+                        Next Clip
                     </button>
                 </div>
                 
