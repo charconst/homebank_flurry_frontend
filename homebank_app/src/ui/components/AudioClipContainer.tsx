ab8fa9fbc5cc88f611fe23cd6ea92b392242ff2b
charconst <44410850+charconst@users.noreply.github.com>
2021-04-06 11:27:10 -0700

    Added audio player to container

--- homebank_app/src/ui/components/AudioClipContainer.tsx
+++ homebank_app/src/ui/components/AudioClipContainer.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import ReactAudioPlayer from 'react-audio-player';
 
 class AudioClipContainer extends React.Component {
     render() {
         return (
             <div className="rounded overflow-hidden shadow-lg text-center">
+                <ReactAudioPlayer controls className="container mx-auto"src="https://storage.googleapis.com/homebank-public-audio/test.mp3"></ReactAudioPlayer>
                 <div className="px-6 py-4">
                 <div className="font-bold text-xl mb-2">HomeBank Audio Clip</div>
                 <p className="text-gray-700 text-base">
