ce68b0d3690082b222093b7f757956dd0e862eb5
charconst <44410850+charconst@users.noreply.github.com>
2021-04-06 12:05:51 -0700

    Moved audio player down, added slight margin

--- homebank_app/src/ui/components/AudioClipContainer.tsx
+++ homebank_app/src/ui/components/AudioClipContainer.tsx
@@ -5,7 +5,6 @@ class AudioClipContainer extends React.Component {
     render() {
         return (
             <div className="rounded overflow-hidden shadow-lg text-center">
-                <ReactAudioPlayer controls className="container mx-auto"src="https://storage.googleapis.com/homebank-public-audio/test.mp3"></ReactAudioPlayer>
                 <div className="px-6 py-4">
                 <div className="font-bold text-xl mb-2">HomeBank Audio Clip</div>
                 <p className="text-gray-700 text-base">
@@ -19,6 +18,7 @@ class AudioClipContainer extends React.Component {
                 Note that the target child in this case is +ageYYMMDD[0:2]+ year(s), +ageYYMMDD[2:4]+ month(s), 
                 and +ageYYMMDD[4:6]+ day(s) old.
                 </p>
+                <ReactAudioPlayer controls className="container mx-auto m-8"src="https://storage.googleapis.com/homebank-public-audio/test.mp3"></ReactAudioPlayer>
                 <div className="flex h-16 flex-wrap justify-center">
                     <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-8 border border-blue-500 hover:border-transparent rounded flex-initial m-2">
                         Prev
