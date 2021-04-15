0ef7e206991987e37052c084c554ebe83a6e91cd
charconst <44410850+charconst@users.noreply.github.com>
2021-04-15 12:16:25 -0700

    Audio clip container loads URL from backend

--- homebank_app/src/ui/components/AudioClipContainer.tsx
+++ homebank_app/src/ui/components/AudioClipContainer.tsx
@@ -3,8 +3,49 @@ import ReactAudioPlayer from 'react-audio-player';
 import AudioClipProgressHeader from '../components/AudioClipProgressHeader';
 import UserRatingContainer from './UserRatingContainer';
 
-class AudioClipContainer extends React.Component {
+interface AudioClipState {
+    error: any,
+    isLoaded: boolean,
+    audioURL: string
+}
+
+class AudioClipContainer extends React.Component<{}, AudioClipState> {
+    constructor(props:any) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            audioURL: ""
+        };
+    }
+
+    componentDidMount() {
+        fetch("http://localhost:8081/api/v1/audioqueue")
+        .then(res => res.json())
+        .then(
+            (result) => {
+                this.setState({
+                    isLoaded: true,
+                    audioURL: result.audioURL
+                })
+            },
+            (error) => {
+                console.log(error);
+                this.setState({
+                    isLoaded: false,
+                    error
+                })
+            }
+        )
+    }
+
     render() {
+        const {error, isLoaded, audioURL} = this.state;
+        if (error) {
+            return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div>Loading...</div>;
+        } 
         return (
             <div className="rounded overflow-hidden shadow-lg text-center">
                 <AudioClipProgressHeader></AudioClipProgressHeader>
@@ -21,7 +62,7 @@ class AudioClipContainer extends React.Component {
                 Note that the target child in this case is +ageYYMMDD[0:2]+ year(s), +ageYYMMDD[2:4]+ month(s), 
                 and +ageYYMMDD[4:6]+ day(s) old.
                 </p>
-                <ReactAudioPlayer controls className="container mx-auto m-8"src="https://storage.googleapis.com/homebank-public-audio/test.mp3"></ReactAudioPlayer>
+                <ReactAudioPlayer controls className="container mx-auto m-8"src={audioURL}></ReactAudioPlayer>
                 <div className="flex h-16 flex-wrap justify-center">
                 <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-8 border border-blue-500 hover:border-transparent rounded flex-initial m-2">
                         Previous Clip
