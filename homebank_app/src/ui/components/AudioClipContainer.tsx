2142bfca2a6abb5dfb6918295b112f5c98709a4c
charconst <44410850+charconst@users.noreply.github.com>
2021-05-05 16:01:57 -0700

    React audio player set to public URL of currently selected audio clip

--- homebank_app/src/ui/components/AudioClipContainer.tsx
+++ homebank_app/src/ui/components/AudioClipContainer.tsx
@@ -117,7 +117,7 @@ class AudioClipContainer extends React.Component<{}, AudioClipState> {
                 Note that the target child in this case is +ageYYMMDD[0:2]+ year(s), +ageYYMMDD[2:4]+ month(s), 
                 and +ageYYMMDD[4:6]+ day(s) old.
                 </p>
-                <ReactAudioPlayer controls className="container mx-auto m-8"src={audioURL}></ReactAudioPlayer>
+                <ReactAudioPlayer controls className="container mx-auto m-8"src={selectedAudioClip?.public_url}></ReactAudioPlayer>
                 <div className="flex h-16 flex-wrap justify-center">
                 <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-8 border border-blue-500 hover:border-transparent rounded flex-initial m-2">
                         Previous Clip
