a3731b8441c69b05b2a9a3add034cafe39f60457
charconst <44410850+charconst@users.noreply.github.com>
2021-05-24 10:59:32 -0700

    Changed app header logo to HBF

--- homebank_app/src/ui/components/AppHeader.tsx
+++ homebank_app/src/ui/components/AppHeader.tsx
@@ -7,7 +7,7 @@ class AppHeader extends React.Component {
     <nav className="bg-white py-2 md:py-4 border-b border-gray-200">
       <div className="container px-4 mx-auto md:flex md:items-center">
         <div className="flex justify-between items-center">
-          <Link to="/" className="font-bold text-xl text-indigo-600">HBK</Link>
+          <Link to="/" className="font-bold text-xl text-indigo-600">HBF</Link>
         </div>
         <div className="sm:flex md:flex flex-col md:flex-row md:ml-auto mt-3 md:mt-0" id="navbar-collapse">
             <button className="py-4 px-4 relative border-2 border-transparent text-gray-800 rounded-full hover:text-gray-400 focus:outline-none focus:text-gray-500 transition duration-150 ease-in-out">
