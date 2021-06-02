94dbc641d9d1e14c866b4a9c3842cb82cdd4aef1
charconst <44410850+charconst@users.noreply.github.com>
2021-06-02 14:12:49 -0700

    Added Button to navigate to home page when user is signed in

--- homebank_app/src/ui/pages/SignInPage.tsx
+++ homebank_app/src/ui/pages/SignInPage.tsx
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import ReactDOM from 'react-dom';
+import { Link } from "react-router-dom";
 import axios from 'axios';
 import GoogleLogin from 'react-google-login';
 import ApiUrl from '../../util/ApiUrl';
@@ -77,6 +78,7 @@ function SignInPage () {
             console.log(err);
         }
     }
+
     return (
         <div>
             {!user && (
@@ -88,32 +90,26 @@ function SignInPage () {
                 />
             )}
             {user && (
-                <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-full m-2" onClick={signOut}>Sign Out</button>
+                <div className="p-4">
+                    
+                    <div className="rounded shadow-lg hero container max-w-screen-lg mx-auto pb-10">
+                        <img className="mx-auto rounded-full border border-gray-100 shadow-sm" src={user.picture}></img>
+                        <h1 className="mx-auto text-lg m-2"><b>{user.name}</b></h1>
+
+                        <div className="w-full h-64 space-y-4 mt-16">
+                            <h1 className="text-2xl font-bold">Great! You're all signed in. </h1>
+                            <p className="font-light m-4 pb-8">Head back to the home page to get started.</p>
+                            <Link to="/" className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">Get Started</Link>
+                        </div>
+
+                        <p className="text-sm m-4 mx-auto">Logged in as: <b>[{user.id}]</b></p>
+                        <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-full m-2" onClick={signOut}>Sign Out</button>
+                    </div>
+                </div>
             )}
             
             {fetchError && (
                 <p style={{color: 'red'}}>{fetchError}</p>
-            )}
-            {user && (
-                <p className="text-xl m-4">Logged in as: <b>[{user.id}]</b></p>
-            )}
-            {user && (
-                <div className="">
-                    <div className="rounded overflow-hidden shadow-lg text-center flex h-32 justify-center m-8">
-                        <div className="flex-col h-16 justify-center m-4 space-x-2">
-                            <div className="relative w-16 h-16">
-                                <img className="rounded-full border border-gray-100 shadow-sm" src={user.picture}></img>
-                            </div>
-                            
-                            <h1 className="text-lg m-2 relative -inset-x-8"><b>{user.name}</b></h1>
-                        </div>
-
-                        <div className="flex-row h-16 justify-center m-4 space-x-2">
-                            <p><i>Account Creation Timestamp: </i><b>{user.creation_timestamp}</b></p>
-                            <p><i>Email Address: </i><b>{user.email}</b></p>
-                        </div>
-                    </div>
-                </div>
             )}
         </div>
     );
