d635a4ce7d041447150340d699a92b239acfc3b7
charconst <44410850+charconst@users.noreply.github.com>
2021-06-14 17:33:22 -0700

    Removed default route placeholder text

--- homebank_app/src/ui/pages/Home.tsx
+++ homebank_app/src/ui/pages/Home.tsx
@@ -6,7 +6,6 @@ class Home extends React.Component {
     render() {
         return (
             <div className="container mx-auto">
-                <h1>/ default route</h1>
                 <AudioClipContainer></AudioClipContainer>
             </div>
         )
