d40fcae6d4093b34b488be06f6638637dcd48de8
charconst <44410850+charconst@users.noreply.github.com>
2021-06-14 17:16:14 -0700

    Removed audio clip progress bar

--- homebank_app/src/ui/components/AudioClipProgressHeader.tsx
+++ homebank_app/src/ui/components/AudioClipProgressHeader.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import AudioClipProgressBar from '../components/AudioClipProgressBar';
 
 interface AudioClipProgressProps {
     totalClips: number,
@@ -11,7 +10,6 @@ class AudioClipProgressHeader extends React.Component<AudioClipProgressProps> {
         return (
             <div className="rounded overflow-hidden bg-gray-50 text-center">
                 <div className="m-2"></div>
-                <AudioClipProgressBar></AudioClipProgressBar>
                 <div className="font-bold text-xl mb-2 mt-2">{this.props.currentClip} of {this.props.totalClips} Clips Reviewed</div>
             </div>
         );
