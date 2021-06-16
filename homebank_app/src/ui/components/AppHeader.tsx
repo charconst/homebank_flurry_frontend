2149d601517b69bda48217b2b8104f380c3a205a
charconst <44410850+charconst@users.noreply.github.com>
2021-06-15 17:44:51 -0700

    Changed Sign In header to toggle to Sign Out/Sign In

--- homebank_app/src/ui/components/AppHeader.tsx
+++ homebank_app/src/ui/components/AppHeader.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import {useRecoilValue} from 'recoil';
+import {useRecoilState, useRecoilValue} from 'recoil';
 import { User } from '../../model/User';
 import {userState, getUserState} from '../../util/AppState';
 
 function AppHeader() {
     const loggedInUser: User = useRecoilValue(getUserState);
+    const [user, setLoggedInUser] = useRecoilState(userState);
+
+    function logOut() {
+      localStorage.removeItem('token');
+      localStorage.removeItem('user_id');
+      setLoggedInUser(new User({}));
+      alert("Signed Out.");
+    }
     return <div className="header-2 bg-yellow-300">
     <nav className="bg-white py-2 md:py-4 border-b border-gray-200">
       <div className="container px-4 mx-auto md:flex md:items-center">
@@ -13,17 +21,15 @@ function AppHeader() {
           <Link to="/" className="font-bold text-xl text-indigo-600">HBF</Link>
         </div>
         <div className="sm:flex md:flex flex-col md:flex-row md:ml-auto mt-3 md:mt-0" id="navbar-collapse">
-            <button className="py-4 px-4 relative border-2 border-transparent text-gray-800 rounded-full hover:text-gray-400 focus:outline-none focus:text-gray-500 transition duration-150 ease-in-out">
-              Preferences
-            </button>
-            <button className="py-4 px-4 relative border-2 border-transparent text-gray-800 rounded-full hover:text-gray-400 focus:outline-none focus:text-gray-500 transition duration-150 ease-in-out">
-              {!loggedInUser.id && (
-                <Link to="/signin" className="text-black text-base">Sign In</Link>
-              )}
-              {loggedInUser.id && (
+            {loggedInUser.id && (
+              <div className="mt-2 space-x-6">
                 <Link to="/signin" className="text-black text-base">{loggedInUser.name}</Link>
-              )}
-            </button>
+                <button onClick={logOut}>Sign Out</button>
+              </div>
+            )}
+            {!loggedInUser.id && (
+              <Link to="/signin" className="text-black text-base">Sign In</Link>
+            )}
         </div>
       </div>
     </nav>
